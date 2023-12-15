de771e609ccde072e9ae633a6e782818be083c10
fseniuk <franciscosseniuk@gmail.com>
2023-12-15 10:23:12 -0300

    Revert "First commit"
    This reverts commit 4e21d2975c560ecc3eefa653ecbca6d0fb8b58f7.

--- src/main/java/com/raspberry/awards/model/CompleteResult.java
+++
@@ -1,6 +0,0 @@
-package com.raspberry.awards.model;
-
-import java.util.Set;
-
-public record CompleteResult(Set<IndividualResult> min, Set<IndividualResult> max) {
-}
--- src/main/java/com/raspberry/awards/repository/MoviesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.raspberry.awards.repository;
-
-import com.raspberry.awards.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoviesRepository extends JpaRepository<Movie, String> {
-}
